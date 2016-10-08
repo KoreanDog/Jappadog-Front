b5f990108bb8493964bb4b3d48c078c2b2c65366
Gary Szeto <gjszeto@gmail.com>
2016-10-08 06:17:03 -0700

    Renamed the controller to Sales

--- application/config/routes.php
+++ application/config/routes.php
@@ -51,8 +51,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 
 $route['production/recipe/:any'] = 'production/recipe';
-$route['Sales/ItemDetail/(:any)'] = 'stockcontrol/itemdetail/$1';
-$route['Sales'] = 'StockControl';
+$route['Sales/ItemDetail/(:any)'] = 'sales/itemdetail/$1';
+$route['Sales'] = 'Sales';
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
--- application/controllers/Sales.php
+++ application/controllers/Sales.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-class StockControl extends Application
+class Sales extends Application
 {
 	/**
 	 * Index Page for this controller.
